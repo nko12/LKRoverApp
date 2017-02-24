2314b5228132aed247b6f8263b1895735a69a571
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-02-24 09:21:28 -0500

    Refactor to use arrays in LKHW

--- lk_rover/include/lk_rover/lk_hw.h
+++ lk_rover/include/lk_rover/lk_hw.h
@@ -1,10 +1,14 @@
 #ifndef LK_HW_H
 #define LK_HW_H
 
+#include <array>
+
+#include "lk_rover/lk_rover.h"
+
 class LKHW {
 public:
-  virtual void setPWMs(double pwms[]);
-  virtual void getCount(double time, double pos[]);
+  virtual void setPWMs(const std::array<double, kNumWheels>&);
+  virtual void getCount(std::array<double, kNumWheels>&);
 };
 
 #endif
