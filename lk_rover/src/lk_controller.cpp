edbae9d953cb1d33cdbd2d0809c5047e560df224
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-04-01 12:07:13 -0400

    Add more skeleton code

--- lk_rover/src/lk_controller.cpp
+++ lk_rover/src/lk_controller.cpp
@@ -1,4 +1,12 @@
 #include "lk_rover/lk_controller.h"
 
 LKController(ros::NodeHandle &nh_, ros::NodeHandle &nhPrivate_): nh(nh_), nhPrivate(nhPrivate_) {
+  // TODO: subscribe to whatever that's needed, set up publishers for the actuators
+}
+
+LKController::~LKController() {
+}
+
+void LKController::doStuff() {
+  // TODO: logic goes here
 }
