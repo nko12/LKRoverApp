03df1df9ee8c3df765552c9b5408e4c85f2bb9be
Lunar Knights <lunarknightsatucf@gmail.com>
2017-04-27 17:59:14 -0400

    Working teleop

--- lk_teleop/src/lk_teleop.cpp
+++ lk_teleop/src/lk_teleop.cpp
@@ -2,10 +2,13 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/types.h>
 #include <linux/joystick.h>
 
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Float64.h"
 // lots of code copied from http://www.cs.uleth.ca/~holzmann/C/system/ttyraw.c
 struct termios origTermios;
 
@@ -15,7 +18,11 @@ int main(int argc, char **argv) {
   auto nh = ros::NodeHandle();
   auto nhPriv = ros::NodeHandle("~");
 
-  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+  ros::Publisher pubBase = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+  ros::Publisher pubLadderSpin = nh.advertise<std_msgs::Float64>("/ladder_spin", 1);
+  ros::Publisher pubBucketLift = nh.advertise<std_msgs::Float64>("/bucket_lift", 1);
+  ros::Publisher pubLadderLift = nh.advertise<std_msgs::Float64>("/ladder_lift", 1);
+  ros::Publisher pubFlapLift = nh.advertise<std_msgs::Float64>("/flap_lift", 1);
 
   int joy_fd = open("/dev/input/js0", O_RDONLY);
   if (joy_fd == -1) {
@@ -35,25 +42,105 @@ int main(int argc, char **argv) {
   ROS_INFO("Found joystick %s with %d axes and %d buttons", joystickName, numAxes, numButtons);
 
   struct js_event js;
+  float bucketPos = 0.0;
+  float ladderPos = 0.0;
+  float flapPos = 0.0; 
+
+  fd_set set;
+  struct timeval timeout;
+
+
   while (1) {
-    if (read(joy_fd, &js, sizeof(js)) < 0) {
-      fprintf(stderr, "unable to read controller\n");
-      exit(-2);
+    // printf("loop\n");
+
+    FD_ZERO(&set);
+    FD_SET(joy_fd, &set);
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 20000;
+    int rv = select(joy_fd + 1, &set, NULL, NULL, &timeout);
+    if (rv > 0) {
+	    if (read(joy_fd, &js, sizeof(js)) < 0) {
+	      fprintf(stderr, "unable to read controller\n");
+	      exit(-2);
+	    }
+	    switch (js.type & ~JS_EVENT_INIT) {
+	      case JS_EVENT_AXIS:
+		axes[js.number] = js.value;
+		if(js.number == 0 || js.number == 1){
+			float forceLin = axes[1];
+			float forceAng = axes[0];
+			if(forceLin <= 1000 && forceLin >= -1000)
+				forceLin = 0;
+			if(forceAng <= 1000 && forceAng >= -1000)
+				forceAng = 0;
+		        geometry_msgs::Twist msg = {};
+		        msg.linear.x = forceLin/(-32767.0);
+		        msg.angular.z = forceAng/32767.0;	
+			pubBase.publish(msg);
+		}
+		if(js.number == 5){
+			float force = axes[5];
+		        std_msgs::Float64 msg;
+
+			if(force >= 0.0)
+		        	msg.data = (force/(32767.0));
+			else
+				msg.data = (0.0);
+			pubLadderSpin.publish(msg);
+		}
+		if(js.number == 2){
+			float force = axes[2];
+			std_msgs::Float64 msg;
+			if(force >= 0)
+				msg.data = (-force/(32767.0));
+			else
+				msg.data = (0.0);
+			pubLadderSpin.publish(msg);
+		}
+		
+	break;
+	      case JS_EVENT_BUTTON:
+		buttons[js.number] = js.value;
+		if(js.number == 8){
+			float pos = buttons[8];
+			std_msgs::Float64 msg;
+			msg.data = pos;
+			pubFlapLift.publish(msg);		
+		}	
+
+		break;
+	    }
+#if 0
+	    for(int i = 0; i < numAxes; ++i)
+	      printf("a %d:%6d ", i, axes[i]);
+	    for(int i = 0; i < numButtons; ++i)
+	      printf("b %d: %d ", i, buttons[i]);
+	    printf("  \r");
+	    fflush(stdout);
+#endif
+    } else if (rv == 0) {
+       // printf("timeout\n");
+       if (buttons[0] || buttons[1] || buttons[2] || buttons[3]) {
+          if (buttons[1] || buttons[3]) {
+            bucketPos += 0.01*(buttons[3] - buttons[1]);
+            if (bucketPos > 1.00) bucketPos = 1.00;
+            if (bucketPos < 0.00) bucketPos = 0.00;
+            std_msgs::Float64 msg;
+            msg.data = bucketPos;
+            pubBucketLift.publish(msg);
+          }
+          if (buttons[2] || buttons[0]) {
+            ladderPos += 0.01*(buttons[2] - buttons[0]);
+            if (ladderPos > 1.00) ladderPos = 1.00;
+            if (ladderPos < 0.00) ladderPos = 0.00;
+            std_msgs::Float64 msg;
+            msg.data = ladderPos;
+            pubLadderLift.publish(msg);
+          }
+       }
+    } else {
+      fprintf(stderr, "select() error\n");
     }
-    switch (js.type & ~JS_EVENT_INIT) {
-      case JS_EVENT_AXIS:
-        axes[js.number] = js.value;
-        break;
-      case JS_EVENT_BUTTON:
-        buttons[js.number] = js.value;
-        break;
-    }
-		for(int i = 0; i < numAxes; ++i)
-			printf("a %d:%6d ", i, axes[i]);
-		for(int i = 0; i < numButtons; ++i)
-			printf("b %d: %d ", i, buttons[i]);
-		printf("  \r");
-		fflush(stdout);
   }
 
   close(joy_fd);
